ef3075c3d30e09c004830b9a2ac0835e14d995aa
Max Hully <max.hully@gmail.com>
2018-11-07 14:10:12 -0500

    Made icons smaller

--- src/Toolbar/BrushColorPicker.js
+++ src/Toolbar/BrushColorPicker.js
@@ -1,19 +1,27 @@
 import { html } from "lit-html";
 
 export default (colors, onInput, activeColor) => html`
-<legend>Color</legend>
-<div class="icon-list color-list">
-${colors.map(
-    color => html`
-    <div class="icon-list__item">
-    <input type="radio" id="brush-color__${
-        color.id
-    }" name="brush-color" value="${color.id}"
-    ?checked=${color.id === activeColor}
-    @input=${onInput}>
-    <div class="icon-list__item__radio" style="background: ${
-        color.color
-    }"></div>
-    </div>`
-)}
-</div>`;
+    <legend>Color</legend>
+    <div class="icon-list color-list">
+        ${
+            colors.map(
+                color => html`
+                    <div class="icon-list__item color-list__item">
+                        <input
+                            type="radio"
+                            id="brush-color__${color.id}"
+                            name="brush-color"
+                            value="${color.id}"
+                            ?checked="${color.id === activeColor}"
+                            @input="${onInput}"
+                        />
+                        <div
+                            class="icon-list__item__radio"
+                            style="background: ${color.color}"
+                        ></div>
+                    </div>
+                `
+            )
+        }
+    </div>
+`;
