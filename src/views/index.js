6d8f65ed5d7fe1152d243b3d48fd968654f347ac
Max Hully <max.hully@gmail.com>
2019-01-31 10:53:24 -0500

    Hide places list at first, in case of JS failure

--- src/views/index.js
+++ src/views/index.js
@@ -5,12 +5,25 @@ import { hydratedPlacesList } from "../components/PlacesList";
 
 export function renderInitialView() {
     const listOfPlaces = hydratedPlacesList();
+    const startDistrictingSection = document.getElementById(
+        "start-districting"
+    );
     render(
         html`
-            ${listOfPlaces.render()}
+            <h2 class="start-districting__title">
+                Where would you like to redistrict?
+            </h2>
+            <div id="places-list">${listOfPlaces.render()}</div>
+            <div class="request-new-place">
+                <h2 class="call-to-request">Looking for somewhere else?</h2>
+                <a href="./request.html" class="call-to-action call-to-request"
+                    >Request a new place to redistrict.</a
+                >
+            </div>
         `,
-        document.getElementById("places-list")
+        startDistrictingSection
     );
+    startDistrictingSection.classList.remove("hidden");
 }
 
 renderInitialView();
