19da7b6e255aa2a70e346315a353b2c34a478140
Max Hully <max.hully@gmail.com>
2019-04-18 14:21:36 -0400

    Delete commented code

--- src/api/auth.js
+++ src/api/auth.js
@@ -33,10 +33,6 @@ export default function initializeAuthContext(client) {
             return user;
         })
     );
-    // .catch(() => {
-    //     signOut();
-    //     return unauthenticatedUser;
-    // });
 }
 
 /**
