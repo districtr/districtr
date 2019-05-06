4145a20d65603ce461d244a5e822f38db8153f41
Max Hully <max.hully@gmail.com>
2019-05-06 11:48:18 -0400

    Show tooltips for population balance

--- src/components/Charts/PopulationDeviation.js
+++ src/components/Charts/PopulationDeviation.js
@@ -2,7 +2,14 @@ import { html } from "lit-html";
 import { roundToDecimal } from "../../utils";
 
 export default population => {
-    const maxPopDev = Math.max(...population.deviations());
+    let deviations = population
+        .deviations()
+        // .filter(d => d != -1) // leave out empty districts
+        .map(d => Math.abs(d));
+    if (deviations.length == 0) {
+        deviations.push(1);
+    }
+    const maxPopDev = Math.max(...deviations);
     return html`
         <div class="ui-option ui-option--slim">
             <dt class="ui-label">Max. population deviation:</dt>
