d50da4324e5c3132fb9473813095c327bee148ec
Max Hully <max.hully@gmail.com>
2019-05-24 17:37:39 -0400

    Cache places response

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,6 +1,4 @@
-import { client } from "./client";
-
 export function listPlaces() {
-    // return fetch("/assets/data/places.json").then(resp => resp.json());
-    return client.get("/places/").then(r => r.json());
+    return fetch("/assets/data/response.json").then(resp => resp.json());
+    // return client.get("/places/").then(r => r.json());
 }
