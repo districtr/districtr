48842594f8566a5760471ef76f8c2b75bf7f0638
Nick Doiron <ndoiron@mapmeld.com>
2019-09-24 14:06:26 -0400

    upgrade dependencies, add response.json testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,23 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const createDefaultConfig = require("@open-wc/testing-karma/default-config");
+const { createDefaultConfig } = require('@open-wc/testing-karma');
 const merge = require("webpack-merge");
 
 module.exports = config => {
     config.set(
         merge(createDefaultConfig(config), {
             files: [
-                // runs all files ending with .test in the test folder,
-                // can be overwritten by passing a --grep flag. examples:
-                //
-                // npm run test -- --grep test/foo/bar.test.js
-                // npm run test -- --grep test/bar/*
-                config.grep ? config.grep : "test/**/*.test.js"
-            ]
+                {
+                    pattern: "test/*.test.js",
+                    type: "module"
+                }
+            ],
+
+            frameworks: ["esm"],
+
+            esm: {
+                nodeResolve: true,
+                compatibility: "all"
+            },
 
             // you can overwrite/extend the config further
         })
