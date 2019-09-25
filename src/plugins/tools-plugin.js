3a2c5649aabac4daab1eec2920b426a9e6b2477b
Nick Doiron <ndoiron@mapmeld.com>
2019-09-25 13:27:29 -0400

    accept CSV and JSON

--- src/plugins/tools-plugin.js
+++ src/plugins/tools-plugin.js
@@ -57,11 +57,12 @@ function exportPlanAsJSON(state) {
     download(`districtr-plan-${serialized.id}.json`, text);
 }
 
-function exportPlanAsAssignmentFile(plan, delimiter = ",", extension = "csv") {
-    const text = Object.keys(plan.assignment)
-        .map(unitId => `${unitId}${delimiter}${plan.assignment[unitId]}`)
+function exportPlanAsAssignmentFile(state, delimiter = ",", extension = "csv") {
+    let text = `"id-${state.place.id}-${state.units.id}"${delimiter}assignment\n`;
+    text += Object.keys(state.plan.assignment)
+        .map(unitId => `${unitId}${delimiter}${state.plan.assignment[unitId]}`)
         .join("\n");
-    download(`assignment-${plan.id}.${extension}`, text);
+    download(`assignment-${state.plan.id}.${extension}`, text);
 }
 
 function getMenuItems(state) {
@@ -88,7 +89,7 @@ function getMenuItems(state) {
         },
         {
             name: "Export as assignment CSV",
-            onClick: () => exportPlanAsAssignmentFile(state.plan)
+            onClick: () => exportPlanAsAssignmentFile(state)
         }
     ];
     return items;
--- src/routes.js
+++ src/routes.js
@@ -73,6 +73,50 @@ export function loadPlanFromJSON(planRecord) {
     });
 }
 
+export function loadPlanFromCSV(assignmentList, state) {
+    let rows = assignmentList.split("\n");
+    let headers = rows[0].split(",");
+    if (
+        headers[0].indexOf("\"id-") === 0
+        && headers[0].split("-").length === 3
+    ) {
+        // new format, verify units match
+        //id-state.place.id-state.units.id
+        let placeId = headers[0].split("-")[1],
+            unitId = headers[0].split("-")[2];
+
+        if (placeId !== state.place.id) {
+            throw new Error("CSV is for a different module (another state or region).");
+        } else if (unitId !== state.units.id) {
+            throw new Error("CSV is for this module but a different divison (e.g. blocks, precincts).");
+        }
+    } else {
+        // old format, no column headers
+        headers = null;
+    }
+    let planRecord = state;
+    planRecord.assignment = {};
+    return listPlaces().then(places => {
+        rows.forEach((row, index) => {
+            if (index > 0 || !headers) {
+                let cols = row.split(","),
+                    key = cols[0] * 1,
+                    val = cols[1] * 1;
+
+                if (key && !isNaN(val)) {
+                    planRecord.assignment[key] = val;
+                }
+            }
+        });
+
+        const place = places.find(p => p.id === planRecord.place.id);
+        return {
+            ...planRecord,
+            place
+        };
+    });
+}
+
 export function loadPlanFromURL(url) {
     return fetch(url)
         .then(r => r.json())
