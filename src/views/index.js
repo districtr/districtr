f7d2500e449f382f27684ae097ee292520ce0498
Nick Doiron <ndoiron@mapmeld.com>
2019-09-27 11:46:56 -0400

    Netlify login widget

--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,6 @@
 import { html, render } from "lit-html";
-import initializeAuthContext, { unauthenticatedUser } from "../api/auth";
+import netlifyIdentity from "netlify-identity-widget";
+// import initializeAuthContext, { unauthenticatedUser } from "../api/auth";
 import { client } from "../api/client";
 import { until } from "lit-html/directives/until";
 import { PlaceMapWithData } from "../components/PlaceMap";
@@ -30,30 +31,58 @@ function renderInitialView() {
 
 export default () => {
     renderInitialView();
-    initializeAuthContext(client).then(user => {
-        clearQueriesFromURL();
-        const signInHeader = document.getElementById("sign-in")
-        // if (user !== unauthenticatedUser) {
-        //     render(
-        //         html`
-        //             <p class="sign-in__link">Hello, ${user.first}!</p>
-        //             <a href="/new" class="button sign-in__link"
-        //                 >Draw a new plan</a
-        //             >
-        //             <a href="/signout" class="button sign-in__link">Sign out</a>
-        //         `,
-        //         signInHeader
-        //     );
-        // } else {
-        //     render(
-        //         html`
-        //             <a href="/signin" class="sign-in__link">Sign in</a>
-        //             <a href="/register" class="button sign-in__link">
-        //                 Create your account</a
-        //             >
-        //         `,
-        //         signInHeader
-        //     );
-        // }
-    });
+    clearQueriesFromURL();
+    function asyncRenderUserLogin (user) {
+        netlifyIdentity.hasDoneInit = true;
+        const signInHeader = document.getElementById("sign-in");
+        if (user && user.email && user.user_metadata) {
+            render(
+                html`
+                    <p class="sign-in__link">
+                        Hello, ${user.user_metadata.full_name}!
+                    </p>
+                    <a href="/new" class="button sign-in__link"
+                        >Draw a new plan</a
+                    >
+                    <a
+                        href="#"
+                        class="button sign-in__link"
+                        @click="${() => netlifyIdentity.logout()}"
+                    >
+                        Sign out
+                    </a>
+                `,
+                signInHeader
+            );
+        } else {
+            render(
+                html`
+                    <a
+                        href="#"
+                        class="sign-in__link"
+                        @click="${() => netlifyIdentity.open('login')}"
+                    >
+                        Sign in
+                    </a>
+                    <a
+                        href="#"
+                        class="button sign-in__link"
+                        @click="${() => netlifyIdentity.open('signup')}"
+                    >
+                        Create your account
+                    </a>
+                `,
+                signInHeader
+            );
+        }
+    }
+    if (netlifyIdentity.hasDoneInit) {
+        asyncRenderUserLogin(netlifyIdentity.currentUser());
+    } else {
+        netlifyIdentity.on('init', asyncRenderUserLogin);
+        netlifyIdentity.init();
+        netlifyIdentity.on('login', asyncRenderUserLogin);
+        netlifyIdentity.on('error', console.error);
+        netlifyIdentity.on('logout', asyncRenderUserLogin);
+    }
 };
