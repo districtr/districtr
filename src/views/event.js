d91bcaa2b69ce29d49bded16a59d83d29309b26b
Nick Doiron <ndoiron@mapmeld.com>
2019-09-30 11:22:07 -0400

    event listing and form

--- src/views/event.js
+++ src/views/event.js
@@ -10,8 +10,47 @@ function EventList() {
     return fetch("/.netlify/functions/eventRead", {
         method: "POST",
         data: JSON.stringify({ shortcode: selectedId })
-    }).then(res => res.json()).then(data => {
-        return JSON.stringify(data.data);
+    })
+    .then(res => res.json())
+    .then(eventlist => {
+        return eventlist.data.map((evt) => {
+            return html`
+                <li class="event">
+                    <a href="/event/${evt.shortcode}">
+                        <h4>${evt.name}</h4>
+                        in ${evt.map}
+                    </a>
+                </li>
+            `;
+        });
+    })
+    .catch(() => {
+        return html`
+            <div>Test Me</div>
+        `;
+    });
+}
+
+function createEvent() {
+    let btn = document.getElementById("event_submit");
+    btn.innerText = "Saving...";
+    btn.disabled = true;
+
+    let name = document.getElementById("event_name").value,
+        map = document.getElementById("event_map").value,
+        shortcode = document.getElementById("event_shortcode").value;
+
+    fetch("/.netlify/functions/eventCreate", {
+        method: "POST",
+        body: JSON.stringify({
+            name: name,
+            shortcode: shortcode,
+            map: map
+        })
+    })
+    .then(res => res.json())
+    .then((e) => {
+        navigateTo("/event/" + shortcode);
     });
 }
 
@@ -19,17 +58,37 @@ export default function renderEventView() {
     const target = document.getElementById("contentful");
     render(
         html`
-            <div class="start-districting start-districting--alone">
-Test
+            <div>
+                ${until(EventList(), "Loading events...")}
             </div>
-            ${until(EventList(), "Loading events...")}
-            <form action="/.netlify/functions/eventCreate" method="POST">
-                <input type="text" name="name" placeholder="Name" value=""/>
-                <br/>
-                <input type="text" name="shortcode" placeholder="/events/__" value=""/>
-                <br/>
-                <input type="submit" value="Add Event"/>
-            </form>
+            <div class="event-form">
+                <li class="option-list__item">
+                    <label class="ui-label">Event Name</label>
+                    <input
+                        id="event_name"
+                        class="text-input"
+                    />
+                </li>
+                <li class="option-list__item">
+                    <label class="ui-label">URL shortcode (/event/___)</label>
+                    <input
+                        id="event_shortcode"
+                        class="text-input"
+                    />
+                </li>
+                <li class="option-list__item">
+                    <select id="event_map">
+                        <option value="nc">North Carolina</option>
+                    </select>
+                </li>
+                <button
+                    id="event_submit"
+                    class="button button--submit button--alternate ui-label"
+                    @click="${createEvent}"
+                >
+                    Add Event
+                </button>
+            </div>
         `,
         target
     );
