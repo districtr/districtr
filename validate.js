dc01f643df462a183e8b7c824426e32fe2e833c5
Nick Doiron <ndoiron@mapmeld.com>
2019-09-30 17:20:10 -0400

    validate.js error appears in CI

--- validate.js
+++ validate.js
@@ -13,7 +13,8 @@ function validateFile() {
             try {
                 data = JSON.parse(data);
             } catch (e) {
-                throw new Error("Could not parse JSON data from response.json");
+                console.error("Could not parse JSON data from response.json");
+                process.exit(1);
             }
             if (Array.isArray(data) && data.length > 0) {
                 async function checkPlan(p) {
@@ -26,40 +27,48 @@ function validateFile() {
                 checkPlan(0);
 
             } else {
-                throw new Error("response.json was not an Array with content");
+                console.error("response.json was not an Array with content");
+                process.exit(1);
             }
         } else {
-            throw new Error("response.json was blank");
+            console.error("response.json was blank");
+            process.exit(1);
         }
     });
 }
 async function validatePlan(plan, index) {
     if (!plan.id) {
-        throw new Error((index + 1) + "-th plan had no id");
+        console.error((index + 1) + "-th plan had no id");
+        process.exit(1);
     }
 
     let repeatId = seenPlanIds.indexOf(plan.id);
     if (repeatId > -1) {
-        throw new Error((index + 1) + "-th plan has same id ("
+        console.error((index + 1) + "-th plan has same id ("
             + plan.id + ") as the " + (repeatId + 1) + "-th");
+        process.exit(1);
     }
     seenPlanIds.push(plan.id);
 
     if (!plan.name) {
-        throw new Error(plan.id + " has no name");
+        console.error(plan.id + " has no name");
+        process.exit(1);
     }
     if (!plan.state) {
-        throw new Error(plan.id + " has no state");
+        console.error(plan.id + " has no state");
+        process.exit(1);
     }
     if (!plan.districtingProblems || !Array.isArray(plan.districtingProblems)) {
-        throw new Error(plan.id + " has no array of districting problems");
+        console.error(plan.id + " has no array of districting problems");
+        process.exit(1);
     }
     plan.districtingProblems.forEach((problem, index) => {
         validateProblem(problem, index, plan.id);
     });
 
     if (!plan.units || !Array.isArray(plan.units)) {
-        throw new Error(plan.id + " has no array of units");
+        console.error(plan.id + " has no array of units");
+        process.exit(1);
     }
     async function checkUnit(index) {
         if (index >= plan.units.length) {
@@ -74,56 +83,65 @@ async function validatePlan(plan, index) {
 
 function validateProblem(problem, index, id) {
     if (!problem.numberOfParts || typeof problem.numberOfParts !== "number") {
-        throw new Error(id + " is missing non-zero numberOfParts on its "
+        console.error(id + " is missing non-zero numberOfParts on its "
             + (index + 1) + "-th districting problem");
+        process.exit(1);
     }
     if (!problem.pluralNoun) {
-        throw new Error(id + " is missing pluralNoun on its "
+        console.error(id + " is missing pluralNoun on its "
             + (index + 1) + "-th districting problem");
+        process.exit(1);
     }
     if (!problem.name) {
-      throw new Error(id + " is missing name on its "
-          + (index + 1) + "-th districting problem");
+        console.error(id + " is missing name on its "
+            + (index + 1) + "-th districting problem");
+        process.exit(1);
     }
 }
 
 async function validateUnits(unit, index, plan) {
     ["id", "name", "unitType"].forEach((check) => {
         if (!unit[check]) {
-            throw new Error(plan.id + " is missing " + check + " on its "
+            console.error(plan.id + " is missing " + check + " on its "
                 + (index + 1) + "-th unit entry");
+            process.exit(1);
         }
     });
     if (!unit.columnSets || !Array.isArray(unit.columnSets)) {
-        throw new Error(plan.id + " is missing columnSets array on its "
+        console.error(plan.id + " is missing columnSets array on its "
             + (index + 1) + "-th unit entry");
+        process.exit(1);
     }
     unit.columnSets.forEach((columnSet, csi) => {
         validateColumnSet(columnSet, csi, index, plan.id);
     });
 
     if (!unit.idColumn || !unit.idColumn.name || !unit.idColumn.key) {
-        throw new Error(plan.id + " is missing a complete idColumn on its "
+        console.error(plan.id + " is missing a complete idColumn on its "
             + (index + 1) + "-th unit entry");
+        process.exit(1);
     }
     if (unit.nameColumn) {
         if (!unit.nameColumn.name || !unit.nameColumn.key) {
-            throw new Error(plan.id + " has a broken nameColumn on its "
-            + (index + 1) + "-th unit entry");
+            console.error(plan.id + " has a broken nameColumn on its "
+                + (index + 1) + "-th unit entry");
+            process.exit(1);
         }
     }
     if (!unit.bounds || unit.bounds.length !== 2
         || unit.bounds[0].length !== 2 || unit.bounds[1].length !== 2
     ) {
-        throw new Error(plan.id + " is missing a complete bounds array on its "
+        console.error(plan.id + " is missing a complete bounds array on its "
             + (index + 1) + "-th unit entry");
+        process.exit(1);
     }
 
     if (!unit.tilesets || !Array.isArray(unit.tilesets)
         || unit.tilesets.length !== 2
     ) {
-        throw new Error(plan.id + " is missing a 2-length tilesets array on its "
+        console.error(plan.id + " is missing a 2-length tilesets array on its "
             + (index + 1) + "-th unit entry");
+        process.exit(1);
     }
 
     if (
@@ -132,18 +150,21 @@ async function validateUnits(unit, index, plan) {
     ) {
         unit.tilesets.forEach((tileset) => {
             if (tileset.source.type !== "vector") {
-                throw new Error(plan.id + " is missing a vector type on a tileset, in "
+                console.error(plan.id + " is missing a vector type on a tileset, in "
                     + (index + 1) + "-th unit entry");
+                process.exit(1);
             }
             if (!tileset.source.url) {
-                throw new Error(plan.id + " is missing a source url on a tileset, in "
+                console.error(plan.id + " is missing a source url on a tileset, in "
                     + (index + 1) + "-th unit entry");
+                process.exit(1);
             }
         });
 
     } else {
-        throw new Error(plan.id + " is missing one of fill + circle tilesets on its "
+        console.error(plan.id + " is missing one of fill + circle tilesets on its "
             + (index + 1) + "-th unit entry");
+        process.exit(1);
     }
 
     async function checkTileset(p) {
@@ -153,8 +174,9 @@ async function validateUnits(unit, index, plan) {
         let tileset = unit.tilesets[p];
         ["source", "sourceLayer"].forEach((check) => {
             if (!tileset[check]) {
-                throw new Error(plan.id + " is missing a tileset's " + check + " on its "
+                console.error(plan.id + " is missing a tileset's " + check + " on its "
                     + (index + 1) + "-th unit entry");
+                process.exit(1);
             }
         });
 
@@ -164,25 +186,30 @@ async function validateUnits(unit, index, plan) {
         try {
             res = await fetch(`https://api.mapbox.com/v4/${tset}?secure&access_token=${mbPublicKey}`);
         } catch(e) {
-            throw new Error("No internet to validate MapBox");
+            console.error("No internet to validate MapBox");
+            process.exit(1);
         }
         let content = await res.json();
         if (content.message) {
-            throw new Error("Error from MapBox Layer " + tset + ":\n" + content.message);
+            console.error("Error from MapBox Layer " + tset + ":\n" + content.message);
+            process.exit(1);
         } else {
             console.log(tset + " modified " + new Date(content.modified));
             // bounds are frequently different
             if (content.filesize < 1000) {
-                throw new Error("MapBox layer " + tset + " was surprisingly small file");
+                console.error("MapBox layer " + tset + " was surprisingly small file");
+                process.exit(1);
             }
             if (content.private) {
-                throw new Error("MapBox layer " + tset + " was private");
+                console.error("MapBox layer " + tset + " was private");
+                process.exit(1);
             }
             let fields = Object.keys(content.vector_layers[0].fields);
 
             // check ID and name column present
             if (!fields.includes(unit.idColumn.key)) {
-                throw new Error("MapBox layer " + tset + " is missing ID column (" + unit.idColumn.key + ")");
+                console.error("MapBox layer " + tset + " is missing ID column (" + unit.idColumn.key + ")");
+                process.exit(1);
             }
             fields.splice(fields.indexOf(unit.idColumn.key), 1);
             if (unit.nameColumn) {
@@ -192,17 +219,20 @@ async function validateUnits(unit, index, plan) {
             unit.columnSets.forEach(cs => {
                 if (cs.total) {
                     if (!fields.includes(cs.total.key)) {
-                        throw new Error("MapBox layer " + tset + " missing total field from response.json (" + subgroup.key + ")");
+                        console.error("MapBox layer " + tset + " missing total field from response.json (" + subgroup.key + ")");
+                        process.exit(1);
                     }
                     fields.splice(fields.indexOf(cs.total.key), 1);
                 }
 
                 cs.subgroups.forEach(subgroup => {
                     if (!fields.includes(subgroup.key)) {
-                        throw new Error("MapBox layer " + tset + " missing field from response.json (" + subgroup.key + ")");
+                        console.error("MapBox layer " + tset + " missing field from response.json (" + subgroup.key + ")");
+                        process.exit(1);
                     }
                     if (content.vector_layers[0].fields[subgroup.key] !== "Number") {
-                        throw new Error("MapBox layer " + tset + " has non-numeric data on " + subgroup.key);
+                        console.error("MapBox layer " + tset + " has non-numeric data on " + subgroup.key);
+                        process.exit(1);
                     }
                     fields.splice(fields.indexOf(subgroup.key), 1);
                 });
@@ -226,9 +256,10 @@ function validateColumnSet(columnSet, columnSetIndex, unitIndex, planId) {
         if (columnSet.metadata &&
             (!columnSet.metadata.race || !columnSet.metadata.year))
         {
-            throw new Error(planId + " is missing election metadata on its "
+            console.error(planId + " is missing election metadata on its "
                 + (unitIndex + 1) + "-th unit entry, "
                 + (columnSetIndex + 1) + "-th columnSet");
+            process.exit(1);
         }
     } else {
         checks.push("name");
@@ -238,16 +269,18 @@ function validateColumnSet(columnSet, columnSetIndex, unitIndex, planId) {
 
     checks.forEach((check) => {
         if (!columnSet[check]) {
-            throw new Error(planId + " is missing a columnSet " + check + " on its "
+            console.error(planId + " is missing a columnSet " + check + " on its "
                 + (unitIndex + 1) + "-th unit entry, "
                 + (columnSetIndex + 1) + "-th columnSet");
+            process.exit(1);
         }
     });
 
     if (!columnSet.subgroups || !Array.isArray(columnSet.subgroups)) {
-        throw new Error(planId + " is missing a subgroups array on its "
+        console.error(planId + " is missing a subgroups array on its "
             + (unitIndex + 1) + "-th unit entry, "
             + (columnSetIndex + 1) + "-th columnSet");
+        process.exit(1);
     }
     columnSet.subgroups.forEach((subgroup, sindex) => {
         validateCSentry(columnSet, sindex, columnSetIndex, unitIndex, planId);
@@ -259,19 +292,20 @@ function validateCSentry(columnSet, field, columnSetIndex, unitIndex, planId) {
         : columnSet[field];
     ["key", "name"].forEach((check) => {
         if (!columnData[check]) {
-            throw new Error(planId + " is missing a " + check + " on its "
+            console.error(planId + " is missing a " + check + " on its "
                 + (unitIndex + 1) + "-th unit entry, "
                 + (columnSetIndex + 1) + "-th columnSet");
+            process.exit(1);
         }
     });
     ["sum", "min", "max"].forEach((check) => {
         if (typeof columnData[check] !== "number") {
-            throw new Error(planId + " is missing a numeric " + check + " on its "
+            console.error(planId + " is missing a numeric " + check + " on its "
                 + (unitIndex + 1) + "-th unit entry, "
                 + (columnSetIndex + 1) + "-th columnSet");
+            process.exit(1);
         }
     });
 }
 
-// start at the top
 validateFile();
