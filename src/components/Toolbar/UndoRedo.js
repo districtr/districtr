7b8ac9dbe097984691a76bf94cd3e28f21712d40
Nick Doiron <ndoiron@mapmeld.com>
2019-12-18 16:19:05 -0500

    button styling

--- src/components/Toolbar/UndoRedo.js
+++ src/components/Toolbar/UndoRedo.js
@@ -1,25 +1,23 @@
 import { html } from "lit-html";
 
 export default (undo, redo) => html`
-    <div class="ui-option">
+    <div class="ui-option" style="padding-top:8px">
         <legend class="ui-label ui-label--row">
             Undo / Redo
         </legend>
-        <div class="slider-container">
+        <div style="text-align: center;width:100%;">
             <button
                 class="button undo-redo"
                 @click="${undo}"
                 style="margin-right:8px"
             >
-                <i class="material-icons" style="color: #000;font-size:16px;">undo</i>
-                Undo
+                <i class="material-icons" style="color: #000;font-size:18px;">undo</i>
             </button>
             <button
                 class="button button--green"
                 @click="${redo}"
             >
-                <i class="material-icons" style="color: #000;font-size:16px;">redo</i>
-                Redo
+                <i class="material-icons" style="color: #000;font-size:18px;">redo</i>
             </button>
         </div>
     </div>
