0f3bbc882e05e85ee7ab9934fd01592f0b757ec8
Nick Doiron <ndoiron@mapmeld.com>
2020-02-26 19:28:44 -0800

    work on serverless

--- src/lambda/districtCenter.js
+++ src/lambda/districtCenter.js
@@ -36,35 +36,30 @@ const states = [
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  try {
-      const client = new Pool({
-          connectionString: process.env.POSTGRES_CONNECT
-      });
-
-      const request = JSON.parse(event.body),
-            ids = request.ids,
-            state = request.state;
-
-      if (!states.includes(state)) {
-          throw new Error("did not recognize state in districtCenter.js");
+  const client = new Pool({
+      connectionString: process.env.POSTGRES_CONNECT,
+      ssl: {
+        rejectUnauthorized: false
       }
-      if (typeof ids !== "string") {
-          throw new Error("expected comma-separated list of unit IDs");
-      }
+  });
 
-      await client.connect();
+  const request = JSON.parse(event.body),
+        ids = event.queryStringParameters.ids,
+        state = event.queryStringParameters.state;
 
-      const res = await client.query('SELECT api.merged_' + state + '($1)', [ids]);
-      client.end();
-      return {
-          statusCode: 201,
-          body: JSON.stringify(res.rows[0])
-      };
-  } catch (err) {
-      console.log('district.center', err) // output to netlify function log
-      return {
-          statusCode: 500,
-          body: JSON.stringify({msg: err.message})
-      }
+  if (!states.includes(state)) {
+      throw new Error("did not recognize state in districtCenter.js");
   }
+  if (typeof ids !== "string") {
+      throw new Error("expected comma-separated list of unit IDs");
+  }
+
+  await client.connect();
+
+  const res = await client.query('SELECT api.merged_' + state + '($1)', [ids]);
+  client.end();
+  return {
+      statusCode: 201,
+      body: JSON.stringify(res.rows[0])
+  };
 }
