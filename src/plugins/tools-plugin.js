5185596dc834db6969f4256d7da672e40ffdaf21
Nick Doiron <ndoiron@mapmeld.com>
2020-03-09 19:27:41 -0400

    remove extra calls to contiguity check

--- src/plugins/tools-plugin.js
+++ src/plugins/tools-plugin.js
@@ -27,44 +27,6 @@ export default function ToolsPlugin(editor) {
         if (planNumbers) {
             planNumbers.update(state, colorsAffected);
         }
-
-        let districts = [],
-            i = 0;
-        while (i < state.problem.numberOfParts) {
-            districts.push(i);
-            i++;
-        }
-        let testIDs = [];
-        Object.keys(state.plan.assignment).forEach((unit_id) => {
-            let districtNum = state.plan.assignment[unit_id];
-            if (testIDs[districtNum]) {
-                testIDs[districtNum].push(unit_id);
-            } else {
-                testIDs[districtNum] = [unit_id];
-            }
-        });
-
-        // let my_tstamp = new Date();
-        colorsAffected.forEach((dnum) => {
-            fetch("/.netlify/functions/planContiguity", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    ids: testIDs[dnum].join(","),
-                    state: state.place.id
-                })
-            })
-            .then(res => res.json())
-            .then((data) => {
-                if (data.length) {
-                    data = data[0];
-                }
-                let keys = Object.keys(data);
-                console.log(dnum + ": " + data[keys[0]]);
-            });
-        });
     });
 
     let tools = [
