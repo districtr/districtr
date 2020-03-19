5d7f960ddd2725e04f2ca401a0eb47c12851f0ed
JN <jenniniels.matthews@gmail.com>
2020-03-19 11:53:36 -0400

    initial oregon demo

---
+++ src/views/oregon.js
@@ -0,0 +1,111 @@
+import { html, render } from "lit-html";
+import { listPlacesForState, getUnits } from "../components/PlacesList";
+import { startNewPlan } from "../routes";
+
+export default () => {
+    listPlacesForState("Oregon").then(places => {
+        const target_or = document.getElementById("districting-options-state");
+        render(districtingOptions(places.filter(p => p.id === "oregon")), target_or);
+        const target_po = document.getElementById("districting-options-local");
+        render(districtingOptions(places.filter(p => p.id === "portlandor")), target_po);
+    });
+    render(plansSection(), document.getElementById("plans"));
+};
+
+const plans = [
+        {
+        title: "Enacted Plans",
+        plans: [
+            {
+                id: "plan_a",
+                name: "Congressional Plan",
+                numbers: []
+            },
+            {
+                id: "plan_b",
+                name: "State Senate Plan",
+                numbers: []
+            },
+            {
+                id: "plan_c",
+                name: "State House Plan",
+                numbers: []
+            }
+        ]
+    },
+];
+
+const plansSection = () =>
+    plans.map(
+        ({ title, plans }) => html`
+            <section class="place__section">
+                <h3>${title}</h3>
+                ${loadablePlans(plans)}
+            </section>
+        `
+    );
+
+const loadablePlans = plans =>
+    html`
+        <ul class="plan-thumbs">
+            ${plans.map(loadablePlan)}
+        </ul>
+    `;
+
+const numberList = numbers => html`
+    <dl class="number-list">
+        ${numbers.map(
+            ({ number, caption }) => html`
+                <div class="number-list__row">
+                    <dt class="number-list__number">${number}</dt>
+                    <dd class="number-list__caption">${caption}</dd>
+                </div>
+            `
+        )}
+    </dl>
+`;
+
+const loadablePlan = plan => html`
+    <a href="/yakima/${plan.id}">
+        <li class="plan-thumbs__thumb">
+            <img
+                class="thumb__img"
+                src="/assets/yakima-plans/${plan.id}.png"
+                alt="Districting Plan ${plan.id}"
+            />
+            <figcaption class="thumb__caption">
+                <h6 class="thumb__heading">${plan.name || plan.id}</h6>
+                ${plan.description ? plan.description : ""}
+                ${plan.numbers ? numberList(plan.numbers) : ""}
+            </figcaption>
+        </li>
+    </a>
+`;
+
+const districtingOptions = places =>
+    html`
+        <ul class="places-list places-list--columns">
+            ${placeItemsTemplate(places[0], startNewPlan)}
+        </ul>
+    `;
+
+const placeItemsTemplate = (place, onClick) =>
+    place.districtingProblems
+        .map(problem =>
+            getUnits(place, problem).map(
+                units => html`
+                    <li
+                        class="places-list__item places-list__item--small"
+                        @click="${() => onClick(place, problem, units)}"
+                    >
+                        <div class="place-name">
+                            ${problem.numberOfParts} ${problem.pluralNoun}
+                        </div>
+                        <div class="place-info">
+                            Built out of ${units.unitType}
+                        </div>
+                    </li>
+                `
+            )
+        )
+        .reduce((items, item) => [...items, ...item], []);
