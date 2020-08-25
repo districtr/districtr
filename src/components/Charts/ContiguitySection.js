366f90662d41605038da7cd3dc3ddc17b23c8bf1
Zhenghong Lieu <lieu@pop-os.localdomain>
2020-08-25 14:32:04 +0800

    Remove unnecessary code in ContiguitySection.js

--- src/components/Charts/ContiguitySection.js
+++ src/components/Charts/ContiguitySection.js
@@ -3,37 +3,10 @@ import { actions } from "../../reducers/charts";
 import { districtColors } from "../../colors";
 
 export default function ContiguitySection(contiguities, uiState, dispatch) {
-  let foundDiscontiguity = false;
-  Object.keys(contiguities).forEach((d) => {
-    if (!contiguities[d]) {
-      foundDiscontiguity = true;
-    }
-  });
   return html`
     <section class="toolbar-section">
       <h4 id="contiguity-status">
-        ${foundDiscontiguity
-          ? "Districts may have contiguity gaps"
-          : "Any districts are contiguous"}
       </h4>
-      <div class="district-row">
-        ${Object.keys(contiguities).map((dnum) => {
-          return html`
-            <div>
-              <span
-                id="contiguity-${dnum}"
-                class="part-number"
-                style="background:${districtColors[dnum].hex};
-                                  display:${contiguities[dnum]
-                  ? "none"
-                  : "flex"};"
-              >
-                ${Number(dnum) + 1}
-              </span>
-            </div>
-          `;
-        })}
-      </div>
     </section>
   `;
 }
