1800dc127068f12503684bf86e98b55fbe2e3a60
Nick Doiron <ndoiron@mapmeld.com>
2020-09-01 11:53:25 -0400

    contiguity check never runs on non-active states

--- src/plugins/tools-plugin.js
+++ src/plugins/tools-plugin.js
@@ -31,13 +31,14 @@ export default function ToolsPlugin(editor) {
     };
 
     let planNumbers = NumberMarkers(state, brush);
-    const c_checker = ContiguityChecker(state, brush);
+    const c_checker = spatial_abilities(state.place.id).contiguity
+        ? ContiguityChecker(state, brush)
+        : null;
     brush.on("colorop", (isUndoRedo, colorsAffected) => {
         savePlanToStorage(state.serialize());
-        if (spatial_abilities(state.place.id).contiguity) {
+        if (c_checker) {
             c_checker(state, colorsAffected);
         }
-
         if (planNumbers) {
             planNumbers.update(state, colorsAffected);
         }
