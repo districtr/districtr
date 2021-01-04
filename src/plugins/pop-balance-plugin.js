3a8dde7347fff69fd5e805c8c96954e76bf02516
Nick Doiron <ndoiron@mapmeld.com>
2021-01-04 10:34:16 -0500

    zoom to unassigned

--- src/plugins/pop-balance-plugin.js
+++ src/plugins/pop-balance-plugin.js
@@ -5,11 +5,49 @@ import MultiMemberPopBalanceChart from "../components/Charts/MMPopBalanceChart";
 import populationBarChart from "../components/Charts/PopulationBarChart";
 import populationDeviation from "../components/Charts/PopulationDeviation";
 import unassignedPopulation from "../components/Charts/UnassignedPopulation";
+import { spatial_abilities } from "../utils";
 
 export default function PopulationBalancePlugin(editor) {
     const problem = editor.state.plan.problem;
     const state = editor.state;
     const tab = new Tab("criteria", "Population", editor.store);
+
+    let plan = editor.state.plan,
+        place = editor.state.place.id,
+        extra_source = (editor.state.units.sourceId === "ma_precincts_02_10") ? "ma_02" : 0;
+    if (editor.state.units.sourceId === "ma_towns") {
+        extra_source = "ma_towns";
+    }
+    const placeID = extra_source || place;
+    const sep = (placeID === "louisiana") ? ";" : ",";
+
+    const zoomToUnassigned = spatial_abilities(editor.state.place.id).contiguity
+      ? (e) => {
+        let saveplan = state.serialize();
+        const GERRYCHAIN_URL = "//mggg.pythonanywhere.com";
+        fetch(GERRYCHAIN_URL + "/unassigned", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(saveplan),
+        })
+        .then((res) => res.json())
+        .catch((e) => console.error(e))
+        .then((data) => {
+          if (data["-1"] && data["-1"].length) {
+            const ids = data["-1"].sort((a, b) => b.length - a.length)[0];
+            fetch(`https://mggg-states.subzero.cloud/rest/rpc/merged_${placeID}?ids=${ids.join(sep)}`).then(res => res.json()).then((centroid) => {
+              centroid = centroid.replace('POINT', '').replace('(', '').replace(')', '').trim().split(" ").map(n => Number(n));
+              editor.state.map.flyTo({
+                center: centroid,
+                zoom: 14
+              });
+            });
+          }
+        });
+      }
+      : null;
 
     if (problem.type === "multimember") {
         tab.addRevealSection(
@@ -18,7 +56,7 @@ export default function PopulationBalancePlugin(editor) {
                 ${MultiMemberPopBalanceChart(state.population, state.parts)}
                 <dl class="report-data-list">
                     ${unassignedPopulation(state.population)}
-                    ${HighlightUnassigned(state.unitsBorders)}
+                    ${HighlightUnassigned(state.unitsBorders, zoomToUnassigned)}
                 </dl>
             `
         );
@@ -31,7 +69,7 @@ export default function PopulationBalancePlugin(editor) {
                     <dl class="report-data-list">
                         ${unassignedPopulation(state.population)}
                         ${populationDeviation(state.population)}
-                        ${HighlightUnassigned(state.unitsBorders)}
+                        ${HighlightUnassigned(state.unitsBorders, zoomToUnassigned)}
                     </dl>
                 `
         );
