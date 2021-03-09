3057410177b12a55a2faeef77a3ebd839dec58bd
JN <jenniniels.matthews@gmail.com>
2021-03-09 11:56:53 -0500

    Only request on vra-score computation at a time.

--- src/map/vra_effectiveness.js
+++ src/map/vra_effectiveness.js
@@ -15,6 +15,9 @@ export default function VRAEffectiveness(state, brush, toolbar) {
     state.vra_effectiveness = {};
   }
 
+  let awaiting_response = false;
+  let cur_request_id = 0;
+
   const vraupdater = (state) => {
     // Object.keys(state.plan.assignment).map((k, i) => {
     //     state.plan.assignment[k] = Array.isArray(state.plan.assignment[k]) ? state.plan.assignment[k][0] : state.plan.assignment[k]
@@ -22,32 +25,41 @@ export default function VRAEffectiveness(state, brush, toolbar) {
     let assign = Object.fromEntries(Object.entries(state.plan.assignment).map(([k, v]) => [k, Array.isArray(v) ? v[0] : v]));
 
     // console.log(state);
-    // console.log(assign)
+    // console.log(assign);
     let saveplan = state.serialize();
     // console.log(JSON.stringify(saveplan));
     const GERRYCHAIN_URL = "//mggg.pythonanywhere.com";
-    fetch(GERRYCHAIN_URL + "/vra", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-          "assignment": assign,
-          "placeId": state.place.id,
-          "groups": state.place.id === "tx_vra" ? ["Hispanic", "Black"] : ["Black"]
-      }),
-    })
-      .then((res) => res.json())
-      .catch((e) => console.error(e))
-      .then((data) => {
-        state.vra_effectiveness = data;
-        // console.log(data);
-        const target = document.getElementById("toolbar");
-        if (target === null) {
-            return;
-        }
-        render(toolbar.render(), target);
-      });
+    
+    if (!awaiting_response) {
+        cur_request_id += 1;
+        awaiting_response = true;
+        fetch(GERRYCHAIN_URL + "/vra", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "assignment": assign,
+                "placeId": state.place.id,
+                "groups": state.place.id === "tx_vra" ? ["Hispanic", "Black"] : ["Black"],
+                "seq_id": cur_request_id
+            }),
+          })
+            .then((res) => res.json())
+            .catch((e) => console.error(e))
+            .then((data) => {
+                if (data["seq_id"] === cur_request_id) {
+                    awaiting_response = false;
+                    state.vra_effectiveness = data["data"];
+                    // console.log(data);
+                    const target = document.getElementById("toolbar");
+                    if (target === null) {
+                        return;
+                    }
+                    render(toolbar.render(), target);
+              }
+        });
+    }
   };
   vraupdater(state);
   return vraupdater;
