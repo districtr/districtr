df0393a226efd9ab23306b47625ad80a3fc98eca
Anthony Pizzimenti <pizzimentianthony@gmail.com>
2021-03-16 14:13:12 -0400

    Wisconsin 2019 ACS Data (#275)

--- src/plugins/pop-balance-plugin.js
+++ src/plugins/pop-balance-plugin.js
@@ -1,3 +1,4 @@
+
 import { html } from "lit-html";
 import { Tab } from "../components/Tab";
 import HighlightUnassigned from "../components/Charts/HighlightUnassigned";
@@ -6,14 +7,14 @@ import populationBarChart from "../components/Charts/PopulationBarChart";
 import populationDeviation from "../components/Charts/PopulationDeviation";
 import unassignedPopulation from "../components/Charts/UnassignedPopulation";
 import { spatial_abilities } from "../utils";
+import populateDatasetInfo from "../components/Charts/DatasetInfo";
 
 export default function PopulationBalancePlugin(editor) {
     const problem = editor.state.plan.problem;
     const state = editor.state;
     const tab = new Tab("criteria", "Population", editor.store);
-
-    let plan = editor.state.plan,
-        place = editor.state.place.id,
+    
+    let place = editor.state.place.id,
         extra_source = (editor.state.units.sourceId === "ma_precincts_02_10") ? "ma_02" : 0;
     if (editor.state.units.sourceId === "ma_towns") {
         extra_source = "ma_towns";
@@ -67,6 +68,9 @@ export default function PopulationBalancePlugin(editor) {
         tab.addRevealSection(
             "Population Balance",
             () => html`
+                <section class="toolbar-inner dataset-info">
+                    ${populateDatasetInfo(state)};
+                </section>
                 ${MultiMemberPopBalanceChart(state.population, state.parts)}
                 <dl class="report-data-list">
                     ${unassignedPopulation(state.population)}
@@ -79,6 +83,9 @@ export default function PopulationBalancePlugin(editor) {
             "Population Balance",
             () =>
                 html`
+                    <section class="toolbar-inner dataset-info">
+                        ${populateDatasetInfo(state)};
+                    </section>
                     ${populationBarChart(state.population, state.activeParts)}
                     <dl class="report-data-list">
                         ${unassignedPopulation(state.population)}
@@ -88,5 +95,7 @@ export default function PopulationBalancePlugin(editor) {
                 `
         );
     }
+    
+    // Add the tab to the toolbar.
     editor.toolbar.addTab(tab);
 }
