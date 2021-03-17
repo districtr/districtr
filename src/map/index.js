20b46608e0f13295bdd03eb5896588fec9679c8d
Jamie Atlas <atlascommaj@gmail.com>
2021-03-16 18:30:53 -0700

    MN counties and Baltimore modules (#279)

--- src/map/index.js
+++ src/map/index.js
@@ -298,7 +298,7 @@ export function addLayers(map, swipemap, parts, tilesets, layerAdder, borderId) 
 
     // cities in Communities of Interest will have a thick border
     if (["austin", "batonrouge", "buncombe", "chicago", "fortworth", "lowell", "ontarioca", "philadelphia", "phoenix", "providence_ri", "rochestermn", "santa_clara", "napa", "napaschools", "portlandor", "kingcountywa", "miamifl", "ccsanitation2", "vabeach",
-    "cincinnati", "clevelandeuclid", "columbus", "dayton", "limaoh", "mansfield", "portsmouthoh", "toledo", "youngstown"
+    "akroncanton", "cincinnati", "clevelandeuclid", "columbus", "dayton", "limaoh", "mansfield", "portsmouthoh", "toledo", "youngstown", "baltimore","stlouis"
     ].includes(borderId)) {
         fetch(`/assets/city_border/${borderId}.geojson`)
             .then(res => res.json())
