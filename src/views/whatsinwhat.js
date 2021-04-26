51d482e63e32fd4f262180e21c99267b7434c72d
Jack Deschler <deschler.jack@gmail.com>
2021-04-26 13:18:09 -0400

    back to full table, js based

--- src/views/whatsinwhat.js
+++ src/views/whatsinwhat.js
@@ -1,56 +1,160 @@
 import { spatial_abilities } from "../utils.js"
-import { html, render } from "lit-html";
-import initializeAuthContext, { unauthenticatedUser } from "../api/auth";
-import { client } from "../api/client";
-import { until } from "lit-html/directives/until";
-import { PlaceMapWithData, PlaceMap } from "../components/PlaceMap";
 
 export default() => {
-    const elt = document.getElementById("choice");
-    elt.addEventListener("change", function () {
-        get_json_file(elt.value);
-    });
-    renderInitialView();
+    var state_table = `
+    <table id = "state-table" class='data'><tbody><tr>
+    <th>Module</th>
+    <th>Coalition Builder</th>
+    <th>Contiguity Checks</th>
+    <th>Current District Boundaries</th>
+    <th>Paint by County</th>
+    <th>Tribal Nations</th>
+    <th>Zoom to Unassigned Units</th>
+    <th>Blocks</th>
+    <th>Block Groups</th>
+    <th>Precincts/Wards</th></tr>`
+    
+
+    var other_table = `
+    <table id = "other-table" class='data'><tbody><tr>
+    <th>Module</th>
+    <th>Coalition Builder</th>
+    <th>Contiguity Checks</th>
+    <th>Current District Boundaries</th>
+    <th>Paint by County</th>
+    <th>Tribal Nations</th>
+    <th>Zoom to Unassigned Units</th>
+    <th>Blocks</th>
+    <th>Block Groups</th>
+    <th>Precincts/Wards</th></tr>`
+    
+    recursive_table_builder(state_table, other_table, 0);
+
+}
+// couple useful globals
+const available_states = [
+    'Alabama','Alaska',
+    'Arizona','Arkansas',
+    'California','Colorado',
+    'Connecticut','Delaware',
+    'Washington, DC','Florida',
+    'Georgia','Hawaii','Idaho',
+    'Illinois','Indiana','Iowa',
+    'Kansas','Kentucky','Louisiana',
+    'Maine','Maryland','Massachusetts',
+    'Michigan','Minnesota','Mississippi',
+    'Missouri','Montana','Nebraska',
+    'Nevada','New Hampshire','New Jersey',
+    'New Mexico','New York','North Carolina',
+    'North Dakota','Ohio','Oklahoma',
+    'Oregon','Pennsylvania',
+    'Puerto Rico','Rhode Island','South Carolina',
+    'South Dakota','Tennessee','Texas','Utah',
+    'Vermont','Virginia',
+    'Washington','West Virginia',
+    'Wisconsin','Wyoming'
+]
+
+const state_name_to_postal = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+    'Washington, DC': 'DC',
+    'Puerto Rico': 'PR'
 }
 
-const get_json_file = (state) => {
-    var str = state.concat(".json");
+function recursive_table_builder(state_table, other_table, index) {
+    // base case, we are done!
+    if (index + 1 > available_states.length) {
+        // close tables
+        state_table = state_table + "</tbody></table>"
+        other_table = other_table + "</tbody></table>"
+
+        // change the DOM
+        var state_table_container = document.getElementById('state-table-div');
+        state_table_container.classList.add('table-container');
+        state_table_container.innerHTML = state_table;
+
+        var other_table_container = document.getElementById('other-table-div');
+        other_table_container.classList.add('table-container');
+        other_table_container.innerHTML = other_table;
+        return;
+    }
+
+    // Recursive case, have to call the next state AFTER fetching
+    const state = available_states[index];
+    const str = state.concat(".json");
     fetch('../assets/data/modules/'.concat(str))
     .then(response => response.json())
-    .then(data => build_table(data, state));
+    .then(data => {
+            const rows = build_table_for_state(data, state);
+            state_table = state_table + rows['state'];
+            other_table = other_table + rows['other'];
+            return recursive_table_builder(state_table, other_table, index + 1);
+    });
 }
 
-const build_table = (data, state) => {
-    var html = `
-        <table class='data'><tbody><tr>
-        <th>Module</th>
-        <th>Coalition Builder</th>
-        <th>Contiguity Checks</th>
-        <th>Current District Boundaries</th>
-        <th>Paint by County</th>
-        <th>Tribal Nations</th>
-        <th>Zoom to Unassigned Units</th>
-        <th>Blocks</th>
-        <th>Block Groups</th>
-        <th>Precincts/Wards</th></tr>`
-        
-
+const build_table_for_state = (data, state) => {
     const properties = ['coalition_builder', 'contiguity_checks', 'current_districts', 'county_brush', 'native_american', 'find_unpainted']
-    
-    // makes the statewide module appear first
-    var statewide;
-    for (const i in data) {
-        const name = data[i]['name'];
-        if (name == state) {
-            statewide = i;
-            break;
-        }
-    }
-    data.splice(0, 0, data.splice(statewide, 1)[0]);
-    
+    var state_html = "";
+    var other_html = "";
+
     for (const module of data) {
         const spatial = spatial_abilities(module['id']);
-        var row = '<tr><td>' + module['name'] + '</td>';
+        
+        var row = 
+        (module['name'] == state) 
+        ? '<tr><td>' + module['name'] + '</td>' 
+        : '<tr><td>' + module['name'] + ', ' + state_name_to_postal[state] + '</td>';
+
         // First do properties
         for (const p of properties) {
             // deal with coalition builder differently since it is opt out
@@ -113,32 +217,14 @@ const build_table = (data, state) => {
         row = row + '<td>' + units['blockgroups'] + '</td>' 
         row = row + '<td>' + units['precincts'] + '</td>' 
         row = row + '</tr>';
-        html = html + row;
+        if (module['name'] == state)
+            state_html = state_html + row;
+        else 
+            other_html = other_html + row;
     }
-    var table_container = document.getElementById('table-div');
-    table_container.classList.add('table-container');
-    table_container.innerHTML = html;
+    return {'state': state_html, 'other': other_html}
 }
 
 function is_numeric(str){
     return /^\d+$/.test(str);
-}
-
-function renderInitialView() {
-    const mapSection = document.getElementById("select-map");
-
-    render(
-        until(PlaceMapWithData(), ""),
-        mapSection
-    );
-    mapSection.classList.remove("hidden");
-    change_listeners();
-}
-
-function change_listeners() {
-    var states = document.getElementById("select-map").getElementsByClassName('state--available');
-    console.log(states);
-    for (let state in states) {
-        console.log(state.id); 
-    }
 }
