f2b42e44470f5252d70512ba2eaced33d5c6bd8c
apizzimenti <pizzimentianthony@gmail.com>
2021-05-04 10:29:28 -0400

    add disable; add optional parameters

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,27 +3,50 @@ import { html, TemplateResult } from "lit-html";
 
 /**
  * Creates a Button object which performs the action specified by ``onChange``.
- * @param {string} label Text inside the Button.
- * @param {string} hoverText Tooltip text to display when the Button is hovered
+ * @param {String} label Text inside the Button.
+ * @param {String} hoverText Tooltip text to display when the Button is hovered
  * over.
  * @param {function(Event)} onClick Callback which consumes an Event.
- * @param {string|number} optionalID Optional HTML identifier.
+ * @param {String|Number} optionalID Optional HTML identifier.
+ * @param {String} classname Optional classname for the Button; defaults to
+ * "button--alternate".
  * @returns {TemplateResult}
  * @constructor
  */
-export function Button(label, hoverText, onClick, optionalID = null) {
-    return html`
-        <label>
-            <button
-                class="button--alternate"
-                title="${hoverText}"
-                type="button" id="${optionalID}"
-                @click="${e => onClick(e)}"
-            >
-                ${label}
-            </button>
-        </label>
-    `;
+export class Button {
+    constructor(
+        onClick,
+        {
+            label="button", hoverText="button", optionalID=null,
+            className="button--alternate"
+        }
+    ) {
+        // Create this object's HTML template.
+        this.template = html`
+            <label>
+                <button
+                    class=${className}
+                    title="${hoverText}"
+                    type="button"
+                    id="${optionalID}"
+                    @click="${e => onClick(e)}"
+                >
+                    ${label}
+                </button>
+            </label>
+        `;
+        
+        // Get the internal object and make it accessible to the user in the
+        // event they need to modify it.
+        this.entity = document.getElementById(optionalID);
+        
+        // Return the TemplateResult so it can be rendered to the screen.
+        return this.template;
+    }
+    
+    disable() {
+        this.entity.disabled = true;
+    }
 }
 
 export default Button;
