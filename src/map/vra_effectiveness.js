79ea26b6d78bb16c1fb931667b55f3e628b26e9e
JN <jenniniels.matthews@gmail.com>
2021-05-06 13:47:34 -0400

    Clean up candidate/election names for MA and add pre-computation section to Dashboard landing page.

--- src/map/vra_effectiveness.js
+++ src/map/vra_effectiveness.js
@@ -11,7 +11,13 @@ export default function VRAEffectiveness(state, brush, toolbar) {
   const placeID = extra_source || place;
   const sep = (state.place.id === "louisiana") ? ";" : ",";
 
-  const groups = state.place.id === "tx_vra" ? ["Hispanic", "Black"] : ["Black"];
+  const group_map = {
+                        "tx_vra": ["Hispanic", "Black"],
+                        "la_vra": ["Black"],
+                        "ma_vra": ["Asian", "Black","Hispanic"]
+                    };
+
+  const groups = group_map[state.place.id];
 
 //   console.log(state);
   if (!state.vra_effectiveness) {
