d81e9d443a6c8aeb9ef2045b55883c79ffadbe24
Peter Horvath <peter.horvath@tufts.edu>
2021-07-01 11:40:16 -0400

    Change note from Census to ACS
    -note under the Population Balance tab should now show 2019 ACS rather than 2010 Census

--- src/components/Charts/DatasetInfo.js
+++ src/components/Charts/DatasetInfo.js
@@ -17,7 +17,7 @@ function datasetInfo(state) {
             pasorobles: "Uses <strong>2019 American Community Survey</strong> population disaggregated from blockgroups by Cooperative Strategies.",
             sacramento: "Uses <strong>projected 2020 population</strong> based on the American Community Survey by National Demographics Corporation",
         },
-        acsLocations = ["wisco2019acs", "grand_county_2", "mn2020acs"];
+        acsLocations = ["wisco2019acs", "grand_county_2", "mn2020acs", "hall_ga"];
     if (acsLocations.includes(place.id.toLowerCase()) || state.units.id.includes("2019") || population.name !== "Population") {
         return `<p><span>&#9432;</span> ${populations.acs}</p>`;
     } else if (["mesaaz", "sanluiso"].includes(place.id)) {
