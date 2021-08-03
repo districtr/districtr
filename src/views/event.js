0f88f1613fd4f0d045ced865b831aad08a5d2084
Jack Deschler <deschler.jack@gmail.com>
2021-08-03 13:51:00 -0400

    removes extra console log

--- src/views/event.js
+++ src/views/event.js
@@ -978,7 +978,6 @@ export default () => {
                 ${plansSection(plans, eventCode)}
                 ${loadExtraPlans ?
                   html`<button id="${button}" @click="${(e) => {
-                      console.log(fetchurl.replace("skip=0", `skip=${drafts ? draftskip+limitNum : skip+limitNum}`));
                       document.getElementById(pinwheel).style.display = "block";
                       document.getElementById(button).disabled = true;
                       fetch((drafts && draftskip == -1) ? fetchurl 
