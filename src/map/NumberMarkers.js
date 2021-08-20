419da28ffbd0a271fdc1a2c72afcd1b707ff4e7b
Nick Doiron <ndoiron@mapmeld.com>
2021-08-20 08:28:32 -0800

    fix number markers for 2020

--- src/map/NumberMarkers.js
+++ src/map/NumberMarkers.js
@@ -195,7 +195,7 @@ export default function NumberMarkers(state, brush, old=false) {
                 if (markers[district_num].length > 100) {
                     markers[district_num] = random.pickset(markers[district_num], 100);
                 }
-                if (!placeID.includes("2020") && state.unitsRecord.name.includes("2020") && state.unitsRecord.name !== "2020 Wards") {
+                if (!placeID.includes("20") && state.unitsRecord.name.includes("2020") && state.unitsRecord.name !== "2020 Wards") {
                     placeID += "_20";
                 }
                 const serverurl = `//mggg.pythonanywhere.com/findCenter?place=${placeID}&`;
