661b385b3430af65dec3c9953ad9857ad2a6ad1a
Nick Doiron <ndoiron@mapmeld.com>
2021-10-06 04:28:48 -0400

    show prison-adjusted data message on cali blockgroups and others

--- src/components/Charts/DatasetInfo.js
+++ src/components/Charts/DatasetInfo.js
@@ -17,6 +17,7 @@ function datasetInfo(state) {
             acs: "Uses <strong>2019 American Community Survey</strong> data",
             mesa: "Uses <strong>2019 American Community Survey</strong> population disaggregated from blockgroups by Redistricting Partners",
             mesa2020: "Uses <strong>2020 Decennial Census</strong> population with processing by Redistricting Partners",
+            ndc_prison_2020: "Uses <strong>2020 Decennial Census</strong> prison-adjusted population with processing by National Demographics Corporation",
             pasorobles: "Uses <strong>2019 American Community Survey</strong> population disaggregated from blockgroups by Cooperative Strategies",
             sacramento: "Uses <strong>projected 2020 population</strong> based on the American Community Survey by National Demographics Corporation"
         },
@@ -36,11 +37,18 @@ function datasetInfo(state) {
         dataset = `<p><span>&#9432;</span> ${populations.pasorobles}`;
     } else if (["sacramento", "ca_sonoma", "ca_pasadena", "ca_goleta", "ca_santabarbara", "ca_marin", "ca_kings", "ca_merced", "ca_fresno", "ca_nevada", "ca_marina", "ca_arroyo", "ca_sm_county", "ca_sanbenito", "ca_cvista", "ca_bellflower", "ca_camarillo", "ca_fresno_ci", "ca_fremont", "lake_el", "ca_chino", "ca_campbell", "ca_vallejo", "ca_oceano", "ca_grover", "ca_buellton", "buenapark", "ca_stockton", "halfmoon", "ca_carlsbad", "ca_richmond", "elcajon", "laverne", "encinitas", "lodi", "pomona", "sunnyvale"].includes(place.id)) {
         dataset = `<p><span>&#9432;</span> ${populations.sacramento}`;
-    } else if ("2020 VTDs" === state.unitsRecord.name && ["virginia", "maryland"].includes(place.id)) {
-        dataset = `<p><span>&#9432;</span> ${populations.census20adj}`;
     } else if (["2020 Block Groups", "2020 Blocks", "2020 Precincts", "2020 VTDs", "2020 Counties"].includes(state.unitsRecord.name)) {
-        dataset = `<p><span>&#9432;</span> ${populations.census20}`;
-    } else { 
+        if (("2020 VTDs" === state.unitsRecord.name && ["virginia", "maryland"].includes(place.id))
+          || (["california", "ca_SanDiego", "ca_contracosta", "ca_sutter", "menlo_park"].includes(state.place.id))) {
+            dataset = `<p><span>&#9432;</span> ${populations.census20adj}`;
+        } else if (["ccsanitation2", "ca_pasadena"].includes(state.place.id)) {
+          // 2020 - NDC - Prison
+          dataset = `<p><span>&#9432;</span> ${populations.ndc_prison_2020}`;
+        } else {
+          // 2020 generic
+          dataset = `<p><span>&#9432;</span> ${populations.census20}`;
+        }
+    } else {
         dataset = `<p><span>&#9432;</span> ${populations.census}`;
     }
 
